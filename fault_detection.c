643bad91bcec0b5b36b84bc75a3b34df532f2ad7
mevlutkorkmazeng <mevlutkorkmaz.eng@gmail.com>
2026-03-06 16:54:08 +0300

    Update fault_detection.c
    Add Kurtosis calculation for peak detection in vibration signals (Titreşim sinyallerinde tepe tespiti için Kurtosis hesaplamasını ekle)

--- fault_detection.c
+++ fault_detection.c
@@ -26,3 +26,29 @@ float calculate_rms(float *signal, int length) {
     // Calculate square root of the mean
     return sqrtf(sum / (float)length);
 }
+
+/**
+ * @brief Calculates the Kurtosis of a signal buffer.
+ * Measures the "peakiness" or impulsiveness of the signal to detect impacts.
+ * * Formula: K = ( (1/n) * sum( (x_i - mean)^4 ) ) / (std_dev^4)
+ * * @param signal Pointer to the vibration sample array.
+ * @param length Number of samples.
+ * @param mean The average value of the signal (calculated beforehand).
+ * @param std_dev The standard deviation of the signal (calculated beforehand).
+ * @return float The calculated Kurtosis value.
+ */
+float calculate_kurtosis(float *signal, int length, float mean, float std_dev) {
+    float sum_fourth_power = 0.0f;
+    
+    for (int i = 0; i < length; i++) {
+        float diff = signal[i] - mean;
+        // Calculate the 4th moment
+        sum_fourth_power += diff * diff * diff * diff;
+    }
+    
+    // The denominator is the 4th power of standard deviation (sigma^4)
+    float denominator = std_dev * std_dev * std_dev * std_dev;
+    
+    // Return Normalized Kurtosis
+    return (sum_fourth_power / (float)length) / denominator;
+}
